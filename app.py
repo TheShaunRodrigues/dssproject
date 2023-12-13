2f46ead9cad5f70355100735b8acd930e801b932
TheShaunRodrigues <114271699+TheShaunRodrigues@users.noreply.github.com>
2023-12-13 08:43:54 +0000

    some more work

--- app.py
+++ app.py
@@ -2,10 +2,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+CREATORS=[
+  {
+    "id":1,
+    "name":"Shaun Rodrigues"
+  },
+  {
+    "id":2,
+    "name":"Chris Ruzario"
+  },
+  {
+  "id":3,
+  "name":"Colin Pereira"
+  }
+]
 
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html",creators=CREATORS)
 
 
 if __name__ == "__main__":
